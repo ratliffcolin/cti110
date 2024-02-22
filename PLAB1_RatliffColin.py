4da1d8e1936d876e5238d26027393d6741d4c9c0
ratliffcolin <160803570+ratliffcolin@users.noreply.github.com>
2024-02-21 20:34:26 -0500

    P1LAB2
    First Submission

---
+++ PLAB1_RatliffColin.py
@@ -0,0 +1,10 @@
+# Colin Ratliff
+# 2/16/2024
+# PLAB1
+# User Commands
+
+##fname = input("Colin:")
+##lname = input("Ratliff:")
+##print(fname + lname)
+##print(fname, lname)
+##print(Hello, fname + lname! Welcome to CTI-110)
